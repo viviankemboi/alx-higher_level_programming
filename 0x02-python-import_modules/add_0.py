9a3276438c78fd50b80b53b80b3247b8e1aaba4f
viviankemboi <jeptooviviank@gmail.com>
2023-08-10 10:19:40 -0700

    import modules

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -10,5 +10,3 @@ def add(a, b):
         The return value. a + b
     """
     return (a + b)
-
-
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def add(a, b):
     """My addition function
 
@@ -49,4 +48,3 @@ def div(a, b):
         The return value. a / b
     """
     return int(a / b)
-
