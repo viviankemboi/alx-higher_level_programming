ad848e7497aa359676f8bad378b2d6d6043b109e
viviankemboi <jeptooviviank@gmail.com>
2023-09-06 04:56:58 -0700

    Test-driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
+"""
+Function that prints My name is <first name> <last name>
 
-"""Defines a name-printing function."""
+
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
+    """
+    Prints a Name
 
-    Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
-    Raises:
-        TypeError: If either of first_name or last_name are not strings.
+    Arguments:
+
+    first_name: must be a string
+
+    last_name: must be a string, if no argument given, then empty by default
+
+    Otherwise a TypeError is raised
+
     """
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
+        msg = "first_name must be a string"
+        raise TypeError(msg)
+
     if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
+        msg = "last_name must be a string"
+        raise TypeError(msg)
+
     print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
+"""
+Function that prints a text with 2 new lines after each of these characters:
+. ? and :
 
-"""Defines a text-indentation function."""
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+    Prints a text replace some characters with 2 newlines
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
+    Arguments:
 
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    text: must be a string
+
+    If not a string a TypeError is raised
